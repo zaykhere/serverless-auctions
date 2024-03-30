a32682ec0c1b2d649f30743079d96171c618b391
zain <zainjhere@gmail.com>
2024-03-31 00:44:13 +0500

    Closing Auctions after one hour

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,20 @@
+import createHttpError from "http-errors";
+import { closeAuction } from "../lib/closeAuction";
+import { getEndedAuctions } from "../lib/getEndedAuctions";
+
 async function processAuctions(event, context) {
-  console.log("Processing Auctions");
+  try {
+    const auctionsToClose = await getEndedAuctions();
+
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
+
+    return {closed: closePromises.length}
+  } catch (error) {
+    console.log(error);
+    throw new createHttpError.InternalServerError(error);
+  }
+
 }
 
 export const handler = processAuctions;
